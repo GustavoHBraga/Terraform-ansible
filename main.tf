29e6aafba5608f88062c4863f49b67703f5b1dc5
Ubuntu <ubuntu@ip-172-31-67-20.ec2.internal>
2024-05-01 18:48:43 +0000

    Primeiro IAC

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "${var.AWS_REGION}"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  key_name = "${var.KEY_NAME}"
+                
+  tags = {
+    Name = "Primeira instância"
+  }
+  connection {
+    user = "${var.INSTANCE_USERNAME}"
+    private_key = "${file("${var.PATH_TO_KEY}")}"
+    host = "${aws_instance.example.public_dns}"
+  }
+}
